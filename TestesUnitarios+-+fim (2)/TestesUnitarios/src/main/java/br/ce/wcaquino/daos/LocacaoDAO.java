d6bf42f5f6f677f2ac5ce0fdad392962d0880c66
Dennis <dennis.py@gentecontrole.com.br>
2023-11-28 16:30:08 -0300

    Alterado estrutura e organização do projeto

--- TestesUnitarios+-+fim (2)/TestesUnitarios/src/main/java/br/ce/wcaquino/daos/LocacaoDAO.java
+++
@@ -1,9 +0,0 @@
-package br.ce.wcaquino.daos;
-
-import br.ce.wcaquino.entidades.Locacao;
-
-public interface LocacaoDAO {
-
-    public void salvar(Locacao locacao);
-
-}
--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,37 +1,194 @@
 package br.ce.wcaquino.servicos;
 
+import static br.ce.wcaquino.matchers.MatchersProprios.caiNumaSegunda;
+import static br.ce.wcaquino.matchers.MatchersProprios.ehHoje;
+import static br.ce.wcaquino.matchers.MatchersProprios.ehHojeComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import br.ce.wcaquino.builders.FilmeBuilder;
+import br.ce.wcaquino.builders.LocacaoBuilder;
+import br.ce.wcaquino.builders.UsuarioBuilder;
+import br.ce.wcaquino.daos.LocacaoDAO;
+import buildermaster.BuilderMaster;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.util.Date;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class LocacaoServiceTest {
 
+	@InjectMocks
+	private LocacaoService service;
+	@Mock
+	private SPCService spc;
+	@Mock
+	private LocacaoDAO dao;
+	@Mock
+	private EmailService email;
 
-    @Test
-    public void teste() {
-        //cenario
-        LocacaoService service = new LocacaoService();
-        Usuario usuario = new Usuario("Usuario 1");
-        Filme filme = new Filme("Filme 1", 2, 5.0);
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		/*service = new LocacaoService();
+		dao = Mockito.mock(LocacaoDAO.class);
+		service.setLocacaoDAO(dao);
+		spc = Mockito.mock(SPCService.class);
+		service.setSpcService(spc);
+		email = Mockito.mock(EmailService.class);
+		service.setEmailService(email);*/ // Comentado pois ao incluir as Anotations os mocks são iniciados automaticamente
 
-        //acao
-        Locacao locacao = service.alugarFilme(usuario, filme);
+	}
+	
+	@Test
+	public void deveAlugarFilme() throws Exception {
+		Assume.assumeFalse(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
+		
+		//cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().comValor(5.0).agora());
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filmes);
+			
+		//verificacao
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(locacao.getDataLocacao(), ehHoje());
+		error.checkThat(locacao.getDataRetorno(), ehHojeComDiferencaDias(1));
+	}
+	
+	@Test(expected = FilmeSemEstoqueException.class) //Este teste aguarda uma Exception
+	public void naoDeveAlugarFilmeSemEstoque() throws Exception{
+		//cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilmeSemEstoque().agora());
+		
+		//acao
+		service.alugarFilme(usuario, filmes);
+	}
+	
+	@Test
+	public void naoDeveAlugarFilmeSemUsuario() throws FilmeSemEstoqueException{
+		//cenario
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().agora());
+		
+		//acao
+		try {
+			service.alugarFilme(null, filmes);
+			Assert.fail();
+		} catch (LocadoraException e) {
+			assertThat(e.getMessage(), is("Usuario vazio"));
+		}
+	}
 
-        //verificacao. Alterado para import static sem a necessidade de incluir o "Assert" e o CoreMatchers na chamada
-        assertEquals(5.00,locacao.getValor(), 0.01);
-        assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	@Test
+	public void naoDeveAlugarFilmeSemFilme() throws FilmeSemEstoqueException, LocadoraException{
+		//cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		
+		exception.expect(LocadoraException.class);
+		exception.expectMessage("Filme vazio");
+		
+		//acao
+		service.alugarFilme(usuario, null);
+	}
+	
+	@Test
+	public void deveDevolverNaSegundaAoAlugarNoSabado() throws FilmeSemEstoqueException, LocadoraException{
+		Assume.assumeTrue(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
+		
+		//cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().agora());
+		
+		//acao
+		Locacao retorno = service.alugarFilme(usuario, filmes);
+		
+		//verificacao
+		assertThat(retorno.getDataRetorno(), caiNumaSegunda());
+		
+	}
 
-        assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(locacao.getValor(), is(not(6.0)));
-    }
+	// Incluido main para testar a utilização do masterBuilder
+	//	public static void main(String[] args){
+	//		new BuilderMaster().gerarCodigoClasse(Locacao.class);
+	//	}
+
+	@Test
+	public void naoDeveAlugarFilmeParaNegativadoSPC() throws FilmeSemEstoqueException {
+
+		//cenario
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().agora());
+
+		Mockito.when(spc.possuiNegativacao(Mockito.any(Usuario.class))).thenReturn(true);
+
+		//ação
+		try {
+			service.alugarFilme(usuario, filmes);
+			Assert.fail();
+		}catch (LocadoraException e){
+			Assert.assertThat(e.getMessage(), is("Usuário Negativado!"));
+		}
+
+		//verificação
+		Mockito.verify(spc).possuiNegativacao(usuario);
+	}
+
+	@Test
+	public void deveEnviarEmailParaLocacoesAtrasados() {
+
+		//cenario
+		Usuario usuario  = UsuarioBuilder.umUsuario().agora();
+		Usuario usuario2  = UsuarioBuilder.umUsuario().comNome("Usuario em dia").agora();
+		Usuario usuario3  = UsuarioBuilder.umUsuario().comNome("Outro atrasado").agora();
+		List<Locacao> locacoes = Arrays.asList(
+				LocacaoBuilder.umLocacao().atrasado().comUsuario(usuario).agora(),
+				LocacaoBuilder.umLocacao().comUsuario(usuario2).agora(),
+				LocacaoBuilder.umLocacao().atrasado().comUsuario(usuario3).agora(),
+				LocacaoBuilder.umLocacao().atrasado().comUsuario(usuario3).agora());//Incluido mais uma situação com o mesmo usuario para validar quantos emails ele recebera
+
+		Mockito.when(dao.obterLocacoesPendentes()).thenReturn(locacoes);
+
+		//ação
+		service.notificarAtrasos();
+
+		//verificação
+		Mockito.verify(email, Mockito.times(3)).notificarAtraso(Mockito.any(Usuario.class));// Verifica a partir da classe Usuario se alguma notificação foi enviada
+		Mockito.verify(email).notificarAtraso(usuario);
+		Mockito.verify(email,Mockito.atLeastOnce()).notificarAtraso(usuario3);//Incluido atLastOnce impede que a verificação falhe, ja que o mesmo usuario receberia mais de 1 email.
+		                                                                      // Outros exemplos:  times(2) * quantos emails ele tem que receber, no caso 2
+																		      // atLast(2) * Pelo menos 2 emails
+																			  // atMost(2) * No maximo 2
+		Mockito.verify(email, Mockito.never()).notificarAtraso(usuario2); //Garante que quem não deveria receber email não esta recebendo com a inclusão do never()
+		Mockito.verifyNoMoreInteractions(email);
+	}
+
+
 }
